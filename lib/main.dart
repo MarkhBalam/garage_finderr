6acde065106b4b04aa422c71f317db09bca9e60d
natashaaaliyah313 <natashaaaliyah313@gmail.com>
2024-07-11 17:37:12 +0300

    added payment system

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:garage_finder/pages/auth_page.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:garage_finder/pages/payments.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
