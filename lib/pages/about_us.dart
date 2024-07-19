ee98fa07af22d149d85c418e401d846452730dee
mugishabalam4 <mugishabalam4@gmail.com>
2024-07-19 14:48:06 +0300

    modified the about us page

--- lib/pages/about_us.dart
+++ lib/pages/about_us.dart
@@ -9,6 +9,7 @@ class AboutUsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.blue,
         title: Text('About Us'),
       ),
       body: Center(
