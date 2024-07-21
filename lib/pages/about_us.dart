b1fd57ef5e2d1bd0174f0e44e9b880c002b78cae
mugishabalam4 <mugishabalam4@gmail.com>
2024-07-21 18:01:56 +0300

    modified the contact us page

--- lib/pages/about_us.dart
+++ lib/pages/about_us.dart
@@ -11,9 +11,141 @@ class AboutUsPage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.blue,
         title: Text('About Us'),
+        centerTitle: true,
+        elevation: 0,
       ),
-      body: Center(
-        child: Text('This is the About Us Page'),
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.blue[100]!, Colors.blue[300]!],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      spreadRadius: 2,
+                      blurRadius: 10,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'About Garage Finder',
+                      style: TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blueGrey[800],
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    Text(
+                      'Garage Finder is dedicated to helping drivers find nearby garages and mechanics easily. Our goal is to ensure that you get the assistance you need promptly, whether it\'s for routine maintenance or emergency breakdowns.',
+                      style:
+                          TextStyle(fontSize: 16, color: Colors.blueGrey[600]),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      spreadRadius: 2,
+                      blurRadius: 10,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Our Mission',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blueGrey[800],
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    Text(
+                      'Our mission is to provide a reliable and efficient platform that connects drivers with local garages and mechanics, ensuring that you receive quality service whenever and wherever you need it.',
+                      style:
+                          TextStyle(fontSize: 16, color: Colors.blueGrey[600]),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      spreadRadius: 2,
+                      blurRadius: 10,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Contact Us',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blueGrey[800],
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    Text(
+                      'If you have any questions, suggestions, or need support, feel free to reach out to us at:',
+                      style:
+                          TextStyle(fontSize: 16, color: Colors.blueGrey[600]),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Email: support@garagefinderapp.com',
+                      style:
+                          TextStyle(fontSize: 16, color: Colors.blueGrey[600]),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      'Phone: +256 706-772-881',
+                      style:
+                          TextStyle(fontSize: 16, color: Colors.blueGrey[600]),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/pages/contact_us.dart
+++ lib/pages/contact_us.dart
@@ -1,8 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ContactUsPage extends StatelessWidget {
   static Route<dynamic> route() {
     return MaterialPageRoute(builder: (context) => ContactUsPage());
+  }
+
+  Future<void> _launchEmail() async {
+    final Uri params = Uri(
+      scheme: 'mailto',
+      path: 'mugishabalam4@gmail.com',
+      query: 'subject=Support%20Request', // Add subject and body here
+    );
+    var url = params.toString();
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      print('Could not launch $url');
+    }
+  }
+
+  Future<void> _launchCall() async {
+    const url = 'tel:+256706772881';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      print('Could not launch $url');
+    }
+  }
+
+  Future<void> _launchWhatsApp() async {
+    const phoneNumber = '+256706772881';
+    final url = 'https://wa.me/$phoneNumber?text=Hello%20Garage%20Finder';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      print('Could not launch $url');
+    }
   }
 
   @override
@@ -11,9 +45,76 @@ class ContactUsPage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.blue,
         title: Text('Contact Us'),
+        centerTitle: true,
       ),
-      body: Center(
-        child: Text('This is the Contact Us Page'),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.3),
+                    spreadRadius: 2,
+                    blurRadius: 10,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Get in Touch',
+                    style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blueGrey[800],
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'If you have any questions or need assistance, please reach out to us. We\'re here to help!',
+                    style: TextStyle(fontSize: 16, color: Colors.blueGrey[600]),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'Contact Us Via',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blueGrey[800],
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  ListTile(
+                    leading: Icon(Icons.email, color: Colors.blue),
+                    title: Text('Email Us'),
+                    subtitle: Text('mugishabalam4@gmail.com'),
+                    onTap: _launchEmail,
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.phone, color: Colors.blue),
+                    title: Text('Call Us'),
+                    subtitle: Text('+256 706772881'),
+                    onTap: _launchCall,
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.message, color: Colors.green),
+                    title: Text('WhatsApp Us'),
+                    subtitle: Text('+256 706772881'),
+                    onTap: _launchWhatsApp,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
