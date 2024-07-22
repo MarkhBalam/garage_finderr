341ee034a849fea8373821a2eea9e8b3edc2a707
mugishabalam4 <mugishabalam4@gmail.com>
2024-07-22 23:13:49 +0300

    modified common car problems page

--- lib/pages/common_car_problems.dart
+++ lib/pages/common_car_problems.dart
@@ -1,20 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:garage_finder/pages/commonproblems/flat_tire.dart';
-
-import 'package:flutter/material.dart';
-import 'package:garage_finder/pages/commonproblems/flat_tire.dart';
 import 'package:garage_finder/pages/commonproblems/dead_battery.dart';
 import 'package:garage_finder/pages/commonproblems/air_filter.dart';
 import 'package:garage_finder/pages/commonproblems/wiper_blade.dart';
 import 'package:garage_finder/pages/commonproblems/fluids.dart';
 import 'package:garage_finder/pages/commonproblems/headlight.dart';
-import 'package:garage_finder/pages/commonproblems/dead_battery.dart';
 import 'package:garage_finder/pages/commonproblems/tirepressure.dart';
 import 'package:garage_finder/pages/commonproblems/fusepage.dart';
 import 'package:garage_finder/pages/commonproblems/headlight_adjustment.dart';
-
-//pulling changes
-var naa = "just";
 
 class CommonCarProblemsPage extends StatelessWidget {
   @override
@@ -24,57 +17,75 @@ class CommonCarProblemsPage extends StatelessWidget {
         title: Text('Common Car Problems'),
         backgroundColor: Colors.blue, // AppBar color set to blue
       ),
-      body: ListView(
+      body: Stack(
         children: [
-          ProblemTile(
-            title: 'Flat Tire',
-            subtitle: 'Instructions for changing a flat tire.',
-            onTap: () => _navigateTo(context, FlatTirePage()),
+          // Background image
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                    'lib/images/common.jpg'), // Path to your background image
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
-          ProblemTile(
-            title: 'Dead Battery',
-            subtitle: 'Instructions for jump-starting a car.',
-            onTap: () => _navigateTo(context, DeadBatteryPage()),
-          ),
-          ProblemTile(
-            title: 'Replace Air Filter',
-            subtitle: 'Instructions for replacing the air filter.',
-            onTap: () => _navigateTo(context, AirFilterPage()),
-          ),
-          ProblemTile(
-            title: 'Wiper Blade Replacement',
-            subtitle: 'Instructions for replacing wiper blades.',
-            onTap: () => _navigateTo(context, WiperBladePage()),
-          ),
-          ProblemTile(
-            title: 'Check and Top Up Fluids',
-            subtitle: 'Instructions for checking and topping up car fluids.',
-            onTap: () => _navigateTo(context, FluidsPage()),
-          ),
-          ProblemTile(
-            title: 'Headlight Replacement',
-            subtitle: 'Instructions for replacing headlights.',
-            onTap: () => _navigateTo(context, HeadlightPage()),
-          ),
-          ProblemTile(
-            title: 'Clean Battery Terminals',
-            subtitle: 'Instructions for cleaning battery terminals.',
-            onTap: () => _navigateTo(context, DeadBatteryPage()),
-          ),
-          ProblemTile(
-            title: 'Check Tire Pressure',
-            subtitle: 'Instructions for checking and adjusting tire pressure.',
-            onTap: () => _navigateTo(context, TirePressurePage()),
-          ),
-          ProblemTile(
-            title: 'Replace Fuse',
-            subtitle: 'Instructions for replacing a blown fuse.',
-            onTap: () => _navigateTo(context, FusePage()),
-          ),
-          ProblemTile(
-            title: 'Adjust Headlights',
-            subtitle: 'Instructions for adjusting your headlights.',
-            onTap: () => _navigateTo(context, HeadlightsAdjustmentPage()),
+          // Main content
+          ListView(
+            padding: EdgeInsets.all(8.0), // Added padding for better visuals
+            children: [
+              ProblemTile(
+                title: 'Flat Tire',
+                subtitle: 'Instructions for changing a flat tire.',
+                onTap: () => _navigateTo(context, FlatTirePage()),
+              ),
+              ProblemTile(
+                title: 'Dead Battery',
+                subtitle: 'Instructions for jump-starting a car.',
+                onTap: () => _navigateTo(context, DeadBatteryPage()),
+              ),
+              ProblemTile(
+                title: 'Replace Air Filter',
+                subtitle: 'Instructions for replacing the air filter.',
+                onTap: () => _navigateTo(context, AirFilterPage()),
+              ),
+              ProblemTile(
+                title: 'Wiper Blade Replacement',
+                subtitle: 'Instructions for replacing wiper blades.',
+                onTap: () => _navigateTo(context, WiperBladePage()),
+              ),
+              ProblemTile(
+                title: 'Check and Top Up Fluids',
+                subtitle:
+                    'Instructions for checking and topping up car fluids.',
+                onTap: () => _navigateTo(context, FluidsPage()),
+              ),
+              ProblemTile(
+                title: 'Headlight Replacement',
+                subtitle: 'Instructions for replacing headlights.',
+                onTap: () => _navigateTo(context, HeadlightPage()),
+              ),
+              ProblemTile(
+                title: 'Clean Battery Terminals',
+                subtitle: 'Instructions for cleaning battery terminals.',
+                onTap: () => _navigateTo(context, DeadBatteryPage()),
+              ),
+              ProblemTile(
+                title: 'Check Tire Pressure',
+                subtitle:
+                    'Instructions for checking and adjusting tire pressure.',
+                onTap: () => _navigateTo(context, TirePressurePage()),
+              ),
+              ProblemTile(
+                title: 'Replace Fuse',
+                subtitle: 'Instructions for replacing a blown fuse.',
+                onTap: () => _navigateTo(context, FusePage()),
+              ),
+              ProblemTile(
+                title: 'Adjust Headlights',
+                subtitle: 'Instructions for adjusting your headlights.',
+                onTap: () => _navigateTo(context, HeadlightsAdjustmentPage()),
+              ),
+            ],
           ),
         ],
       ),
@@ -103,11 +114,20 @@ class ProblemTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
-      subtitle: Text(subtitle),
-      leading: Icon(Icons.build, color: Colors.blue), // Build icon for problems
-      onTap: onTap,
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 4.0), // Margin between tiles
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.blue, width: 3.0), // Thick border
+        borderRadius: BorderRadius.circular(12.0), // Rounded corners
+        color: Colors.white.withOpacity(0.9), // Slightly transparent background
+      ),
+      child: ListTile(
+        title: Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
+        subtitle: Text(subtitle),
+        leading:
+            Icon(Icons.build, color: Colors.blue), // Build icon for problems
+        onTap: onTap,
+      ),
     );
   }
 }
