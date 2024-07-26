185b6049d7a29219c042ffa10bd984800b383f14
Kirabo Madrine <kirabomadrine68@gmail.com>
2024-07-26 18:31:07 +0300

    modified notification page

--- lib/pages/notifications.dart
+++ lib/pages/notifications.dart
@@ -1,28 +1,4 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Garage Finder'),
-        ),
-        body: NotificationPage(),
-      ),
-    );
-  }
-}
 
 class NotificationPage extends StatefulWidget {
   const NotificationPage({super.key});
@@ -32,45 +8,61 @@ class NotificationPage extends StatefulWidget {
 }
 
 class _NotificationPageState extends State<NotificationPage> {
-  FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
-  String _notificationText = 'No notifications yet';
-
-  @override
-  void initState() {
-    super.initState();
-
-    _firebaseMessaging.requestPermission(
-      sound: true,
-      badge: true,
-      alert: true,
-      provisional: false,
-    );
-
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      print('Got a message whilst in the foreground!');
-      print('Message data: ${message.data}');
-
-      if (message.notification != null) {
-        print('Message also contained a notification: ${message.notification}');
-        setState(() {
-          _notificationText = message.notification!.title ?? 'No Title';
-        });
-      }
-    });
-
-    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-      print('A new onMessageOpenedApp event was published!');
-      print('Message data: ${message.data}');
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        _notificationText,
-        style: TextStyle(fontSize: 20.0),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Notifications"),
+        backgroundColor: Colors.blue,
       ),
-    );
-  }
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.blue.shade100, Colors.blue.shade300],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Display the text
+                Text(
+                  'No notifications here yet.',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue.shade900,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 30), // Add space between text and GIF
+                // Display the GIF
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 10,
+                        offset: Offset(0, 4),
+                      ),
+                    ],
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.asset('lib/gif/waving.gif'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+     ),
+);
+}
 }
