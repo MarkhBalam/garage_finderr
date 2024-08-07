ebf21006017a486db135d5a41cf5cfb8054d5165
Kirabo Madrine <kirabomadrine68@gmail.com>
2024-08-08 01:37:19 +0300

    notifications

--- lib/pages/notifications.dart
+++ lib/pages/notifications.dart
@@ -13,7 +13,7 @@ class _NotificationPageState extends State<NotificationPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Notifications"),
-        backgroundColor: const Color.fromARGB(255, 17, 17, 17),
+        backgroundColor: Colors.blue,
       ),
       body: Container(
         width: double.infinity,
