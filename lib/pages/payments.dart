7620024cb9717fbc51b15c8f30f1e02849a0285b
natashaaaliyah313 <natashaaaliyah313@gmail.com>
2024-08-12 19:00:54 +0300

    adjusted

--- lib/pages/payments.dart
+++ lib/pages/payments.dart
@@ -1,5 +1,54 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';  // Import the intl package
+import 'package:flutterwave_standard/flutterwave.dart';
+import 'package:flutterwave_standard/models/requests/customer.dart';
+import 'package:flutterwave_standard/models/responses/charge_response.dart';
+import 'package:intl/intl.dart'; // Import the intl package
+
+class PaymentService {
+  final String publicKey = 'FLWPUBK_TEST-e931b80b1f9dc244f8f9466593f25269-X';
+  final String secretKey = 'FLWSECK_TEST-2765a8ccd0ebbe629792bb9314f4e1ef-X';
+
+  Future<bool> initiatePayment({
+    required BuildContext context,
+    required String amount,
+    required String currency,
+    required String email,
+    required String txRef,
+    required String phoneNumber,
+  }) async {
+    final Customer customer = Customer(
+      name: "User",
+      phoneNumber: phoneNumber,
+      email: email,
+    );
+
+    final Flutterwave flutterwave = Flutterwave(
+      context: context,
+      publicKey: publicKey,
+      currency: currency,
+      amount: amount,
+      customer: customer,
+      paymentOptions: "mobilemoneyuganda",
+      customization: Customization(title: "Live Payment"),
+      txRef: txRef,
+      isTestMode: true, // Change to false for live payments
+      redirectUrl: "https://your-redirect-url.com", // You can use any URL for testing
+    );
+
+    try {
+      final ChargeResponse response = await flutterwave.charge();
+      if (response != null && response.status == "successful") {
+        return true;
+      } else {
+        print("Transaction failed");
+        return false;
+      }
+    } catch (error) {
+      print("An error occurred: $error");
+      return false;
+    }
+  }
+}
 
 class PaymentPage extends StatefulWidget {
   static route() => MaterialPageRoute(builder: (context) => PaymentPage());
@@ -50,7 +99,9 @@ class _PaymentPageState extends State<PaymentPage> {
           if (method == 'Mobile Money') {
             Navigator.push(
               context,
-              MaterialPageRoute(builder: (context) => MobileMoneyPage()),
+              MaterialPageRoute(
+                builder: (context) => MobileMoneyPage(operator: method),
+              ),
             );
           } else if (method == 'Cash') {
             Navigator.push(
@@ -96,20 +147,56 @@ class _PaymentPageState extends State<PaymentPage> {
 }
 
 class MobileMoneyPage extends StatefulWidget {
+  final String operator;
+
+  MobileMoneyPage({required this.operator});
+
   @override
   _MobileMoneyPageState createState() => _MobileMoneyPageState();
 }
 
 class _MobileMoneyPageState extends State<MobileMoneyPage> {
   String _selectedOperator = '';
+  final TextEditingController _phoneNumberController = TextEditingController();
+  final TextEditingController _amountController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
 
-  void _handlePayment() {
+  @override
+  void initState() {
+    super.initState();
+    _selectedOperator = widget.operator;
+  }
+
+  void _handlePayment() async {
     if (_selectedOperator.isNotEmpty) {
-      // Call your payment API here with the selected operator
-      print('Selected operator: $_selectedOperator');
-      // TODO: Add API call here
+      // Validate the input fields
+      if (_phoneNumberController.text.isEmpty || _amountController.text.isEmpty || _emailController.text.isEmpty) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Please fill in all fields')),
+        );
+        return;
+      }
+
+      final paymentService = PaymentService();
+      bool result = await paymentService.initiatePayment(
+        context: context,
+        amount: _amountController.text,
+        currency: 'UGX',
+        email: _emailController.text,
+        txRef: DateTime.now().millisecondsSinceEpoch.toString(),
+        phoneNumber: _phoneNumberController.text,
+      );
+
+      if (result) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Payment successful')),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Payment failed')),
+        );
+      }
     } else {
-      // Show a message to select an operator
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Please select a mobile money operator')),
       );
@@ -120,7 +207,7 @@ class _MobileMoneyPageState extends State<MobileMoneyPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Select Mobile Money Operator'),
+        title: Text('Mobile Money Payment'),
         backgroundColor: Colors.blue,
       ),
       body: Container(
@@ -134,9 +221,52 @@ class _MobileMoneyPageState extends State<MobileMoneyPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            Text(
+              'Operator: ${_selectedOperator}',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
             SizedBox(height: 20),
-            _buildOperatorOption('Airtel', Colors.red),
-            _buildOperatorOption('MTN', Colors.yellow),
+            TextField(
+              controller: _phoneNumberController,
+              keyboardType: TextInputType.phone,
+              decoration: InputDecoration(
+                labelText: 'Phone Number',
+                filled: true,
+                fillColor: Colors.white.withOpacity(0.8),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              controller: _amountController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Amount',
+                hintText: 'UGX e.g. 40,000',
+                filled: true,
+                fillColor: Colors.white.withOpacity(0.8),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                suffixText: 'UGX',
+              ),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              controller: _emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                labelText: 'Email',
+                hintText: 'e.g. name@email.com',
+                filled: true,
+                fillColor: Colors.white.withOpacity(0.8),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+              ),
+            ),
             Spacer(),
             ElevatedButton(
               onPressed: _handlePayment,
@@ -154,88 +284,9 @@ class _MobileMoneyPageState extends State<MobileMoneyPage> {
       ),
     );
   }
-
-  Widget _buildOperatorOption(String operator, Color color) {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          _selectedOperator = operator;
-        });
-      },
-      child: Container(
-        margin: EdgeInsets.symmetric(vertical: 8.0),
-        decoration: BoxDecoration(
-          color: Colors.white.withOpacity(0.8),
-          borderRadius: BorderRadius.circular(12.0),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black12,
-              blurRadius: 6.0,
-              offset: Offset(0, 3),
-            ),
-          ],
-        ),
-        child: ListTile(
-          leading: Icon(Icons.radio_button_checked, color: color),
-          title: Text(
-            operator,
-            style: TextStyle(
-              color: Colors.black87,
-              fontWeight: FontWeight.bold,
-              fontSize: 18,
-            ),
-          ),
-          trailing: Icon(Icons.arrow_forward_ios, color: Colors.grey),
-        ),
-      ),
-    );
-  }
 }
 
-class CashPaymentPage extends StatefulWidget {
-  @override
-  _CashPaymentPageState createState() => _CashPaymentPageState();
-}
-
-class _CashPaymentPageState extends State<CashPaymentPage> {
-  int _amount = 0;
-  final NumberFormat _currencyFormat = NumberFormat.currency(symbol: 'UGX');
-
-  void _incrementAmount() {
-    setState(() {
-      if (_amount < 1000000) {
-        _amount++;
-      }
-    });
-  }
-
-  void _decrementAmount() {
-    setState(() {
-      if (_amount > 0) _amount--;
-    });
-  }
-
-  void _submitAmount() {
-    // Show a "thank you" message
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Thank You'),
-          content: Text('Amount submitted: ${_currencyFormat.format(_amount)}'),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('OK'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
+class CashPaymentPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -243,103 +294,14 @@ class _CashPaymentPageState extends State<CashPaymentPage> {
         title: Text('Cash Payment'),
         backgroundColor: Colors.blue,
       ),
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('lib/images/payment.jpg'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    padding: const EdgeInsets.all(16.0),
-                    decoration: BoxDecoration(
-                      color: Colors.white.withOpacity(0.8),
-                      borderRadius: BorderRadius.circular(12.0),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black12,
-                          blurRadius: 6.0,
-                          offset: Offset(0, 3),
-                        ),
-                      ],
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                          'Enter Amount',
-                          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox(height: 20),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            IconButton(
-                              icon: Icon(Icons.add, size: 30), // Increment button
-                              onPressed: _incrementAmount,
-                            ),
-                            Text(
-                              _currencyFormat.format(_amount),
-                              style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-                            ),
-                            IconButton(
-                              icon: Icon(Icons.remove, size: 30), // Decrement button
-                              onPressed: _decrementAmount,
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Spacer(),
-            ElevatedButton(
-              onPressed: _submitAmount,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue,
-                padding: EdgeInsets.symmetric(vertical: 15),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12.0),
-                ),
-              ),
-              child: Text('Submit', style: TextStyle(fontSize: 18)),
-            ),
-          ],
-        ),
+      body: Center(
+        child: Text('Proceed to pay with cash.'),
       ),
     );
   }
 }
 
-class CreditCardPage extends StatefulWidget {
-  @override
-  _CreditCardPageState createState() => _CreditCardPageState();
-}
-
-class _CreditCardPageState extends State<CreditCardPage> {
-  final _formKey = GlobalKey<FormState>();
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _amountController = TextEditingController();
-
-  void _submitDetails() {
-    if (_formKey.currentState!.validate()) {
-      // Handle submission
-      String email = _emailController.text;
-      int amount = int.parse(_amountController.text);
-      print('Email: $email');
-      print('Amount: $amount');
-      // TODO: Add your API call here
-    }
-  }
-
+class CreditCardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -347,103 +309,11 @@ class _CreditCardPageState extends State<CreditCardPage> {
         title: Text('Credit/Debit Card Payment'),
         backgroundColor: Colors.blue,
       ),
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('lib/images/payment.jpg'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              SizedBox(height: 20),
-              Text(
-                'Please Fill in Some Details',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black54,
-                ),
-              ),
-              SizedBox(height: 20),
-              _buildEmailField(),
-              SizedBox(height: 20),
-              _buildAmountField(),
-              Spacer(),
-              ElevatedButton(
-                onPressed: _submitDetails,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue,
-                  padding: EdgeInsets.symmetric(vertical: 15),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12.0),
-                  ),
-                ),
-                child: Text('Submit', style: TextStyle(fontSize: 18)),
-              ),
-            ],
-          ),
-        ),
+      body: Center(
+        child: Text('Proceed to pay with a credit/debit card.'),
       ),
-    );
-  }
-
-  Widget _buildEmailField() {
-    return TextFormField(
-      controller: _emailController,
-      keyboardType: TextInputType.emailAddress,
-      decoration: InputDecoration(
-        labelText: 'Email',
-        hintText: 'e.g. name@email.com',
-        filled: true,
-        fillColor: Colors.white.withOpacity(0.8),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12.0),
-        ),
-      ),
-      validator: (value) {
-        if (value == null || value.isEmpty) {
-          return 'Please enter your email';
-        }
-        if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
-          return 'Please enter a valid email address';
-        }
-        return null;
-      },
-    );
-  }
-
-  Widget _buildAmountField() {
-    return TextFormField(
-      controller: _amountController,
-      keyboardType: TextInputType.number,
-      decoration: InputDecoration(
-        labelText: 'Amount',
-        hintText: 'UGX eg 40,000',
-        filled: true,
-        fillColor: Colors.white.withOpacity(0.8),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12.0),
-        ),
-        suffixText: 'UGX',
-      ),
-      validator: (value) {
-        if (value == null || value.isEmpty) {
-          return 'Please enter an amount';
-        }
-        int? amount = int.tryParse(value);
-        if (amount == null) {
-          return 'Please enter a valid amount';
-        }
-        if (amount < 500 || amount > 8000000) {
-          return 'Amount must be between 500 and 8,000,000 UGX';
-        }
-        return null;
-      },
     );
   }
 }
+
+
